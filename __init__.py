ef7df11044e70546c2d1ceb67a7372dfab05f129
mfg637 <mfg636@gmail.com>
2021-12-10 02:42:22 +0200

    add get tag by alias function

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
 from . import files_by_tag_search
+from . import common
+
+def get_tag_name_by_alias(alias):
+    common.open_connection_if_not_opened()
+    cursor = common.connection.cursor()
+    sql_template = "SELECT title FROM tag WHERE ID = (SELECT tag_id FROM tag_alias WHERE title=%s)"
+    cursor.execute(sql_template, (alias,))
+    result = cursor.fetchone()[0]
+    common.close_connection_if_not_closed()
+    return result
