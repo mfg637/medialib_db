c562b0734d8e8e7bc8c98d55bd75263d67a140ac
mfg637 <mfg636@gmail.com>
2022-01-08 20:26:09 +0200

    add get_tags_ids stored procedure

--- sql_scripts/create_tables.sql
+++ sql_scripts/create_tables.sql
@@ -16,12 +16,18 @@ create table tag
 (
     ID       bigint unsigned auto_increment
         primary key,
-    title    varchar(250)                                                                               null,
+    title    varchar(250)                                                                                            null,
     category enum ('artist', 'set', 'copyright', 'original character', 'rating', 'species', 'content', 'characters') null,
+    parent   bigint unsigned                                                                                         null,
     constraint uniq_tag
-        unique (title, category)
+        unique (title, category),
+    constraint tag_ibfk_1
+        foreign key (parent) references tag (ID)
 )
     collate = utf8mb4_unicode_ci;
+
+create index parent
+    on tag (parent);
 
 create table content_tags_list
 (
@@ -46,3 +52,20 @@ create table tag_alias
 )
     charset = utf16;
 
+CREATE PROCEDURE get_tags_ids (IN tag_aliace_name VARCHAR(251))
+BEGIN
+    # taken from https://stackoverflow.com/a/33737203
+    with recursive get_tags_ids_r (id, parent_id) as (
+        select      ID,
+                    parent
+        from        tag
+        where       ID = (SELECT tag_id from tag_alias where tag_alias.title=tag_aliace_name COLLATE utf8mb4_unicode_ci)
+        union all
+        select      t.ID,
+                    t.parent
+        from        tag t
+        inner join  get_tags_ids_r as t_rec
+                on  t.parent = t_rec.ID
+    )
+    select ID from get_tags_ids_r;
+end;
