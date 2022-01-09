e7c0bd3e4822492758ff876633bdea62819bbf0a
mfg637 <mfg636@gmail.com>
2022-01-09 02:59:53 +0200

    undefined problem mysql.connector.errors.DatabaseError: 2014 (HY000): Commands out of sync; you can't run this command now

--- files_by_tag_search.py
+++ files_by_tag_search.py
@@ -1,5 +1,6 @@
 import pathlib
 import enum
+import random
 
 try:
     from . import config
@@ -27,11 +28,19 @@ ordering_constants = {
 
 
 def get_files_with_every_tag(*tags: str, limit: int = None, offset: int = None, order_by: ORDERING_BY = ORDERING_BY.NONE):
-    get_image_id_by_tag_code_block = ("id in (SELECT content_id from content_tags_list where tag_id = "
-                                      "(SELECT tag_id from tag_alias where title=%s))")
+    get_image_id_by_tag_code_block = "id in (SELECT content_id from content_tags_list where tag_id = %s)"
     base_sql_code_block = "SELECT file_path from content where "
     result_sql_block = base_sql_code_block
-    for i in range(len(tags)):
+    get_tags_ids = "CALL get_tags_ids(%s)"
+    tag_ids = list()
+
+    common.open_connection_if_not_opened()
+    cursor = common.connection.cursor()
+    for tag in tags:
+        cursor.execute(get_tags_ids, (tag,))
+        tag_ids.extend([i[0] for i in cursor.fetchall()])
+
+    for i in range(len(tag_ids)):
         result_sql_block += get_image_id_by_tag_code_block
         if i + 1 < len(tags):
             result_sql_block += " AND "
@@ -41,14 +50,15 @@ def get_files_with_every_tag(*tags: str, limit: int = None, offset: int = None, 
         result_sql_block += " LIMIT {}".format(limit)
         if offset is not None:
             result_sql_block += " OFFSET {}".format(offset)
-    common.open_connection_if_not_opened()
-    cursor = common.connection.cursor()
-    cursor.execute(result_sql_block, tags)
+    print(result_sql_block, tag_ids, type(tag_ids[0]))
+    cursor.execute(result_sql_block, tag_ids)
     list_files = list()
     file_path = cursor.fetchone()
     while file_path is not None:
         list_files.append(pathlib.Path(file_path[0]))
         file_path = cursor.fetchone()
+    if order_by == ORDERING_BY.RANDOM:
+        random.shuffle(list_files)
     common.close_connection_if_not_closed()
     return list_files
 
