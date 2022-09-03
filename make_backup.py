a4c3c340e225425743e48d43a1f8c5eb08a47e25
mfg637 <mfg636@gmail.com>
2022-09-03 15:47:17 +0300

    database serialisation

--- make_backup.py
+++ make_backup.py
@@ -1,6 +1,8 @@
 import dataclasses
 import datetime
 import pathlib
+import json
+from typing import Any
 
 import common
 
@@ -23,6 +25,13 @@ class ContentDocument:
     is_hidden: bool
     tags: set[TagUnique]
 
+    def json_serializable(self) -> dict[str, Any]:
+        serializable = dataclasses.asdict(self)
+        serializable["file_path"] = str(self.file_path)
+        serializable["addition_date"] = self.addition_date.isoformat()
+        serializable["tags"] = [dataclasses.asdict(tag) for tag in self.tags]
+        return serializable
+
 
 @dataclasses.dataclass
 class TagDocument:
@@ -31,12 +40,21 @@ class TagDocument:
     aliases: set[str]
     parent: TagUnique | None
 
+    def json_serializable(self) -> dict[str, Any]:
+        serializable = dataclasses.asdict(self)
+        serializable["aliases"] = list(self.aliases)
+        if self.parent is not None:
+            serializable["parent"] = dataclasses.asdict(self.parent)
+        return serializable
+
 
 tags_uniq: set[TagUnique] = set()
 tag_documents = []
 
 
 def main():
+    global tag_documents
+
     content_documents = []
 
     sql_get_content = "SELECT * FROM content order by RAND() LIMIT 5"
@@ -90,12 +108,17 @@ def main():
             origin=content[6],
             origin_content_id=content[7],
             is_hidden=content[8],
-            tags=tags,
-            file_hash=""
+            tags=tags
         ))
-    print(content_documents)
+    content_documents_serializable = [
+        content_document.json_serializable() for content_document in content_documents
+    ]
+    tag_documents_serializable = [
+        tag_document.json_serializable() for tag_document in tag_documents
+    ]
+    print(json.dumps(content_documents_serializable))
     print()
-    print(tag_documents)
+    print(json.dumps(tag_documents_serializable))
 
     connection.close()
 
