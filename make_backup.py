dd7c3b72a36dcf4ef48d394962bdd2637c691f3a
mfg637 <mfg636@gmail.com>
2022-11-14 19:34:02 +0200

    use tar files

--- make_backup.py
+++ make_backup.py
@@ -1,8 +1,12 @@
 import dataclasses
 import datetime
+import io
 import pathlib
 import json
 from typing import Any
+import argparse
+import tarfile
+import crcmod
 
 import common
 
@@ -49,15 +53,29 @@ class TagDocument:
 
 
 tags_uniq: set[TagUnique] = set()
-tag_documents = []
+crc64 = crcmod.predefined.mkCrcFun("crc-64")
+checksums: list[tuple[int, pathlib.PurePath]] = []
+
+
+def create_tar_file(tar_dump: tarfile.TarFile, buffer: io.StringIO, path: pathlib.PurePath, write_crc=True):
+    global checksums
+    encoded_data = buffer.getvalue().encode("utf-8")
+    if write_crc:
+        data_crc64 = crc64(encoded_data)
+        checksums.append((data_crc64, path))
+    data_buffer = io.BytesIO(encoded_data)
+    tar_tag_info = tarfile.TarInfo(str(path))
+    tar_tag_info.size = len(encoded_data)
+    tar_dump.addfile(tar_tag_info, data_buffer)
 
 
 def main():
-    global tag_documents
+    global checksums
 
-    content_documents = []
+    tar_dump = tarfile.TarFile("medialib-dump.tar", "w")
+    tag_uniq_id: dict[TagUnique, int] = dict()
 
-    sql_get_content = "SELECT * FROM content order by RAND() LIMIT 5"
+    sql_get_content = "SELECT * FROM content order by RANDOM() LIMIT 5"
     sql_get_tag_ids = "SELECT tag_id FROM content_tags_list WHERE content_id=%s"
     sql_get_tag = "SELECT * FROM tag WHERE ID=%s"
     sql_get_tag_aliases = "SELECT title FROM tag_alias WHERE tag_id=%s"
@@ -81,14 +99,17 @@ def main():
                 parent_tag_uniq = tags_processing(tag_raw_data[3], cursor)
             cursor.execute(sql_get_tag_aliases, (tag_raw_data[0],))
             tag_aliases = set([raw_alias[0] for raw_alias in cursor.fetchall()])
-            tag_documents.append(
-                TagDocument(
-                    title=tag_raw_data[1],
-                    category=tag_raw_data[2],
-                    aliases=tag_aliases,
-                    parent=parent_tag_uniq
-                )
+            tag_document = TagDocument(
+                title=tag_raw_data[1],
+                category=tag_raw_data[2],
+                aliases=tag_aliases,
+                parent=parent_tag_uniq
             )
+            tag_uniq_id[tag_uniq] = tag_id
+            tag_document_io = io.StringIO()
+            json.dump(tag_document.json_serializable(), tag_document_io)
+            tag_document_path = pathlib.PurePath("tags/{}.json".format(tag_id))
+            create_tar_file(tar_dump, tag_document_io, tag_document_path)
             tags_uniq.add(tag_uniq)
         return tag_uniq
 
@@ -99,7 +120,7 @@ def main():
         tag_ids = cursor.fetchall()
         for tag_id_wrapped in tag_ids:
             tags.add(tags_processing(tag_id_wrapped[0], cursor))
-        content_documents.append(ContentDocument(
+        content_document = ContentDocument(
             file_path=pathlib.Path(content[1]),
             title=content[2],
             content_type=content[3],
@@ -109,17 +130,28 @@ def main():
             origin_content_id=content[7],
             is_hidden=content[8],
             tags=tags
-        ))
-    content_documents_serializable = [
-        content_document.json_serializable() for content_document in content_documents
-    ]
-    tag_documents_serializable = [
-        tag_document.json_serializable() for tag_document in tag_documents
-    ]
-    print(json.dumps(content_documents_serializable))
-    print()
-    print(json.dumps(tag_documents_serializable))
+        )
 
+        content_document_io = io.StringIO()
+        json.dump(content_document.json_serializable(), content_document_io)
+        content_document_path = pathlib.PurePath("content-metadata/{}.json".format(content_id))
+        create_tar_file(tar_dump, content_document_io, content_document_path)
+    tag_uniq_id_io = io.StringIO()
+    tag_uniq_id_serialisable = []
+    for elem in tag_uniq_id:
+        _tag_uniq = dataclasses.astuple(elem)
+        tag_uniq_id_serialisable.append((_tag_uniq[0], _tag_uniq[1], tag_uniq_id[elem]))
+    json.dump(tag_uniq_id_serialisable, tag_uniq_id_io)
+    tag_uniq_id_filepath = pathlib.PurePath("tag_uniq_id.json")
+    create_tar_file(tar_dump, tag_uniq_id_io, tag_uniq_id_filepath)
+
+    checksums_io = io.StringIO()
+    for checksum in checksums:
+        checksums_io.write("{} {}\n".format(hex(checksum[0]).replace("0x", ""), checksum[1]))
+    checksums_filepath = pathlib.PurePath("checksums-crc64")
+    create_tar_file(tar_dump, checksums_io, checksums_filepath, write_crc=False)
+
+    tar_dump.close()
     connection.close()
 
 
