418336de9d9119523df3397ce7a065d890105265
mfg637 <mfg636@gmail.com>
2025-03-30 04:41:16 +0300

    add ability to make requests without tags

--- files_by_tag_search.py
+++ files_by_tag_search.py
@@ -1,3 +1,4 @@
+import logging
 import pathlib
 import enum
 import random
@@ -12,6 +13,8 @@ try:
     from . import common
 except ImportError:
     import common
+
+logger = logging.getLogger(__name__)
 
 
 class ORDERING_BY(enum.Enum):
@@ -36,13 +39,13 @@ class HIDDEN_FILTERING(enum.Enum):
 
 hidden_filtering_constants = {
     HIDDEN_FILTERING.SHOW: "",
-    HIDDEN_FILTERING.FILTER: " AND hidden=FALSE",
-    HIDDEN_FILTERING.ONLY_HIDDEN: " AND hidden=TRUE"
+    HIDDEN_FILTERING.FILTER: "hidden=FALSE",
+    HIDDEN_FILTERING.ONLY_HIDDEN: "hidden=TRUE"
 }
 
 
 def _requests_fabric(
-        *tags_groups: dict[str, typing.Any],
+        *tags_groups: dict[str, typing.Any] | None,
         limit: int = None,
         offset: int = None,
         order_by: ORDERING_BY = ORDERING_BY.NONE,
@@ -53,43 +56,47 @@ def _requests_fabric(
     get_image_id_by_tag_code_block = "id in (SELECT content_id from content_tags_list where tag_id in ({}))"
     get_image_id_by_not_tag_code_block = "id not in (SELECT content_id from content_tags_list where tag_id in ({}))"
     result_sql_block = base_sql_block
-
     tag_ids = list()
-    tags_count = list()
 
-    sql_get_tag_ids = "SELECT * FROM get_tags_ids(%s)"
-    sql_get_parent_ids = "SELECT * FROM get_parent_tag_ids(%s)"
-    for tags_group in tags_groups:
-        raw_tag_ids = []
-        for tag in tags_group["tags"]:
-            if type(tag) is str:
-                cursor.execute(sql_get_tag_ids, (tag,))
-            elif type(tag) is int:
-                cursor.execute(sql_get_parent_ids, (tag,))
-            data = cursor.fetchall()
-            raw_tag_ids.extend(data)
-        tags_count.append(len(raw_tag_ids))
-        _tag_ids = set()
-        for raw_id in raw_tag_ids:
-            _tag_ids.add(raw_id[0])
-        tag_ids.extend(_tag_ids)
+    len_of_groups = len(tags_groups)
+    if len_of_groups:
+        tags_count = list()
 
-    tags_set_lists = list()
-    for i, val in enumerate(tags_count):
-        if tags_groups[i]["not"]: # not tag
-            result_sql_block += get_image_id_by_not_tag_code_block
-        else:
-            result_sql_block += get_image_id_by_tag_code_block
-        tag_set_list = "%s"
-        for j in range(1, val):
-            tag_set_list += ", %s"
-        tags_set_lists.append(tag_set_list)
-        if i + 1 < len(tags_count):
-            result_sql_block += " AND "
+        sql_get_tag_ids = "SELECT * FROM get_tags_ids(%s)"
+        sql_get_parent_ids = "SELECT * FROM get_parent_tag_ids(%s)"
+        for tags_group in tags_groups:
+            raw_tag_ids = []
+            for tag in tags_group["tags"]:
+                if type(tag) is str:
+                    cursor.execute(sql_get_tag_ids, (tag,))
+                elif type(tag) is int:
+                    cursor.execute(sql_get_parent_ids, (tag,))
+                data = cursor.fetchall()
+                raw_tag_ids.extend(data)
+            tags_count.append(len(raw_tag_ids))
+            _tag_ids = set()
+            for raw_id in raw_tag_ids:
+                _tag_ids.add(raw_id[0])
+            tag_ids.extend(_tag_ids)
 
-    result_sql_block = result_sql_block.format(*tags_set_lists)
+        tags_set_lists = list()
+        for i, val in enumerate(tags_count):
+            if tags_groups[i]["not"]: # not tag
+                result_sql_block += get_image_id_by_not_tag_code_block
+            else:
+                result_sql_block += get_image_id_by_tag_code_block
+            tag_set_list = "%s"
+            for j in range(1, val):
+                tag_set_list += ", %s"
+            tags_set_lists.append(tag_set_list)
+            if i + 1 < len(tags_count):
+                result_sql_block += " AND "
+
+        result_sql_block = result_sql_block.format(*tags_set_lists)
 
     if filter_hidden != HIDDEN_FILTERING.SHOW:
+        if len_of_groups:
+            result_sql_block += " AND "
         result_sql_block += hidden_filtering_constants[filter_hidden]
 
     if order_by != ORDERING_BY.NONE:
@@ -98,7 +105,11 @@ def _requests_fabric(
         result_sql_block += " LIMIT {}".format(limit)
         if offset is not None:
             result_sql_block += " OFFSET {}".format(offset)
-    print(result_sql_block, tag_ids, type(tag_ids[0]))
+    logger.debug(
+        "Resulting SQL: %s, tag_ids = %s",
+        result_sql_block,
+        tag_ids
+    )
     cursor.execute(result_sql_block, tag_ids)
 
 
@@ -114,6 +125,35 @@ def get_media_by_tags(
     base_sql_code_block = "SELECT ID, file_path, content_type, title from content where "
     _requests_fabric(
         *tags,
+        limit=limit,
+        offset=offset,
+        order_by=order_by,
+        base_sql_block=base_sql_code_block,
+        cursor=cursor,
+        filter_hidden=filter_hidden
+    )
+
+    list_files = list()
+    file_path = cursor.fetchone()
+    while file_path is not None:
+        list_files.append(file_path)
+        file_path = cursor.fetchone()
+    if order_by == ORDERING_BY.RANDOM:
+        random.shuffle(list_files)
+    connection.close()
+    return list_files
+
+
+def get_all_media(
+        limit: int = None,
+        offset: int = None,
+        order_by: ORDERING_BY = ORDERING_BY.NONE,
+        filter_hidden: HIDDEN_FILTERING = HIDDEN_FILTERING.FILTER,
+    ):
+    connection = common.make_connection()
+    cursor = connection.cursor()
+    base_sql_code_block = "SELECT ID, file_path, content_type, title from content where "
+    _requests_fabric(
         limit=limit,
         offset=offset,
         order_by=order_by,
